210f9bfbe545e72ed13eada03cb1fc4510505a97
mohamedosama2074 <iosengks74@gmail.com>
2019-08-21 13:34:03 +0200

    PanGestureRecognizer

--- PanGestureRecognizer/ViewController.swift
+++ PanGestureRecognizer/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var blueView: UIView!
+    @IBOutlet weak var graeenView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        let gesture = UIPanGestureRecognizer(target: self, action: #selector(ViewController.handlePanGesture(sender:)))
+        
+        graeenView.addGestureRecognizer(gesture)
+        
+    }
+    
+    
+    @objc func handlePanGesture(sender:UIPanGestureRecognizer){
+        
+        let pointOfView = sender.location(in: view)
+        
+        graeenView.center = pointOfView
+        
+        print(pointOfView)
+        
+        
     }
 
 
